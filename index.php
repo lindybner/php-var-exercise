de28f4414352f167b2276f14ed9e42b2a80932ca
Jim Lin-Dybner <lin.jycl@gmail.com>
2024-01-15 13:10:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,17 @@
 </head>
 
 <body>
-    <?php
+    <h1>PHP Variable Exercise</h1>
+    <h2>Instructions</h2>
+    <p>$var = 'PHP Tutorial'. Put this variable into the title section, h3 tag and as an anchor text within an HTML document.</p>
 
+    <?php
+    $var = 'PHP Tutorial';
     ?>
+
+    <h3><?= $var ?></h3>
+    <p>PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language. It is a cross-platform, HTML embedded server-side scripting language and is especially suited for web development.</p>
+    <p><a href="https://www.w3resource.com/php/php-home.php">Go to the <?= $var ?>.</a></p>
 
 </body>
 
